8469028e6004fab6cf586543a2106c9ba6532c62
ionutgit <ionut_chirita@yahoo.com>
2024-04-16 11:01:36 +0300

    navigation component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
+  const Shop = () => {
+    return <h1>I am the shop pages</h1>;
+  }
+
 
   return (
     <Routes>
-      <Route path='/' element={    <Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />}/>
+      </Route>
     </Routes>
 
   )
